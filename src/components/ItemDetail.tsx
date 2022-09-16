098bf994026c02c7f16f4369073a6fde5747997b
RichardIrala <richardirala47@hotmail.com>
2022-09-16 11:14:05 -0300

    Se creo el slider de productos + su css importado para html

--- src/components/ItemDetail.tsx
+++ src/components/ItemDetail.tsx
@@ -1,32 +1,49 @@
 import React from "react";
-
+import Slider from "react-slick";
 export const ItemDetail = (props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
   return (
     <div
       style={{
         boxSizing: "border-box",
-        display: "flex",
         flexWrap: "wrap",
         background: "#817d81",
-        padding: "20px",
-        width: "fit-content",
+        padding: "40px",
         margin: "auto",
         alignItems: "center",
         marginTop: "20px",
         marginBottom: "20px",
-        gap: "10px",
+        gap: "40px",
+        overflow: "hidden",
+        width: "400px",
       }}
     >
-      {props.pictures.map((imagen, index) => (
-        <img
-          key={index}
-          src={imagen.url}
-          alt="producto"
-          style={{ width: "90px", height: "90px" }}
-        />
-      ))}
+      <Slider {...settings}>
+        {props.pictures.map((imagen, index) => (
+          <img
+            key={index}
+            src={imagen.url}
+            alt="producto"
+            style={{ width: "70vw", height: "auto" }}
+          />
+        ))}
+      </Slider>
       <div
-        style={{ display: "flex", flexDirection: "column", maxWidth: "400px" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "400px",
+          marginTop: 50,
+        }}
       >
         <h1>{props.title}</h1>
         <span>$ {props.price}</span>
