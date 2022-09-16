0e4e12d6f36027af3d79ac8b4cbc7def1a1ed63a
RichardIrala <richardirala47@hotmail.com>
2022-09-16 15:15:06 -0300

    Se eliminaron los estilos en línea y se agregaron estilos a través de módulos

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -1,43 +1,23 @@
 import React from "react";
-
 import { useNavigate } from "react-router-dom";
+import styles from "./SearchResultItem.css";
 
 export const SearchResultItem = (props) => {
   const navigate = useNavigate();
   return (
-    <div
-      style={{
-        boxSizing: "border-box",
-        display: "flex",
-        flexWrap: "wrap",
-        background: "#817d81",
-        padding: "20px",
-        width: "fit-content",
-        margin: "auto",
-        alignItems: "center",
-        marginTop: "20px",
-        marginBottom: "20px",
-        gap: "10px",
+    <button
+      className={styles.container}
+      onClick={() => {
+        navigate("/items/" + props.id);
       }}
     >
-      <button
-        onClick={() => {
-          navigate("/items/" + props.id);
-        }}
-      >
-        Ver más...
-      </button>
-      <img
-        src={props.thumbnail}
-        alt="producto"
-        style={{ width: "90px", height: "90px" }}
-      />
-      <div
-        style={{ display: "flex", flexDirection: "column", maxWidth: "400px" }}
-      >
-        <h1>{props.title}</h1>
-        <span>$ {props.price}</span>
+      <div className={styles.secundaryContainer}>
+        <img className={styles.image} src={props.thumbnail} alt="producto" />
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>{props.title}</h1>
+          <span className={styles.textContent}>$ {props.price}</span>
+        </div>
       </div>
-    </div>
+    </button>
   );
 };
